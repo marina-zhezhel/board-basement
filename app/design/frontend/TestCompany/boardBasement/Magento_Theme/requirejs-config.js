f2853ef39845f313493cb92b10299ac3f2a0e0d7
Maryna <maryna.zhezhel@gmail.com>
2018-09-14 13:43:11 +0000

    Add cms home page layout, templates, css

---
+++ app/design/frontend/TestCompany/boardBasement/Magento_Theme/requirejs-config.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved. See COPYING.txt for license
+ * details.
+ */
+
+var config = {
+	"map" : {
+		'*' : {
+			"jquery-3.3.1.min" : "Magento_Theme/js/jquery-3.3.1.min",
+			"jquery.cycle2" : "Magento_Theme/js/jquery.cycle2",
+			"jquery.cycle2.carousel" : "Magento_Theme/js/jquery.cycle2.carousel"
+			
+		}
+	}
+};
